334a2d7109fae183ad37ffd54fce46ed65a22261
Ykyki4 <jimmypantalones@gmail.com>
2022-09-13 18:32:41 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,7 +1,7 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render, get_object_or_404
-from datacenter.views_helpers import format_duration, is_strange
+from datacenter.views_helpers import format_duration, is_strange, get_duration
 
 
 def passcard_info_view(request, passcode):
@@ -11,7 +11,7 @@ def passcard_info_view(request, passcode):
     this_passcard_visits = []
     for visit in all_visits:
         if visit.leaved_at is not None:
-            time_spent = visit.leaved_at - visit.entered_at
+            time_spent = get_duration(visit.entered_at, visit.leaved_at)
         duration = format_duration(time_spent)
         time_spent_minutes = time_spent.total_seconds() // 60
 
